858dadf54cec93e745da01ddda59ec19ab74e51a
FuryGox <furyghost07@gmail.com>
2025-12-09 17:29:03 +0700

    Add MT5 installation script and necessary configuration files

---
+++ MT5/install.bat
@@ -0,0 +1,133 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "base=%APPDATA%\MetaQuotes\Terminal"
+set index=0
+
+:: === COPY FILE ===
+set "botFile=setup\lumir-mt5.ex5"
+echo "%botFile%"
+if not exist "%botFile%" (
+  echo [LOI] Khong tim thay file bot: "%botFile%" !
+  pause
+  exit /b
+)
+
+echo ==== DANH SACH INSTANCE MT5 ====
+echo.
+for /d %%F in ("%base%\*") do (
+  set /a index+=1
+  set "instance!index!=%%F"
+  set "foldername=%%~nF"
+  rem Bỏ qua 3 folder mặc định
+  if /I "!foldername!"=="Common"  (
+    rem skip
+    ) else if /I "!foldername!"=="Community" (
+    rem skip
+    ) else if /I "!foldername!"=="Help" (
+    rem skip
+    ) else if exist "%%F\MQL5" (
+    if exist "%%F\origin.txt" (
+      echo +-------------------------- [Instance !index!] --------------------------+
+      type "%%F\origin.txt"
+      echo.
+      echo +------------------------------------------------------------------+
+    )
+  )
+  echo.
+)
+
+if !index!==0 (
+  echo Khong tim thay instance MT4 nao!
+  pause
+  exit /b
+)
+
+echo.
+set /p choice="Chon so instance de sua TradeHistoryPeriod: "
+
+set "selected=!instance%choice%!"
+if not defined selected (
+  echo Lua chon khong hop le
+  pause
+  exit /b
+)
+
+set "configFile=%selected%\config\terminal.ini"
+
+if not exist "%configFile%" (
+  echo Khong tim thay terminal.ini
+  pause
+  exit /b
+)
+
+echo Dang sua HistoryPeriod tai:
+echo %configFile%
+echo.
+
+set "tempFile=%configFile%.tmp"
+set found=0
+
+(
+for /f "usebackq delims=" %%A in ("%configFile%") do (
+  set "line=%%A"
+  if /I "!line:~0,18!"=="TradeHistoryPeriod" (
+    echo TradeHistoryPeriod=7
+    set "found=1"
+  ) else (
+    echo !line!
+  )
+)
+) > "%tempFile%"
+
+if "!found!"=="0" (
+  echo [Setting]>>"%tempFile%"
+  echo TradeHistoryPeriod=7>>"%tempFile%"
+)
+
+echo Da sua thanh cong TradeHistoryPeriod=7
+
+move /y "%tempFile%" "%configFile%" >nul
+
+echo === FILE BOT ===
+set "targetExperts=!selected!\MQL5\Experts"
+
+if not exist "!targetExperts!" (
+  echo [Thong bao] Tao thu muc Experts: "!targetExperts!"
+  md "!targetExperts!"
+)
+
+copy /y "%botFile%" "!targetExperts!" >nul
+echo Da copy bot thanh cong toi: "!targetExperts!"
+echo.
+
+echo === Config ===
+set "sourceFile=setup\experts.ini"
+if not exist "!sourceFile!" (
+  echo [LOI] Khong tim thay file config: "!sourceFile!" !
+  pause
+  exit /b
+)
+
+rem Dùng biến !selected! để xác định thư mục đích
+set "targetConfig=!selected!\config"
+
+rem Tao thu muc config neu chua ton tai
+if not exist "!targetConfig!" (
+  md "!targetConfig!"
+)
+
+rem Xoa file experts.ini cu
+set "fileToDelete=experts.ini"
+if exist "!targetConfig!\!fileToDelete!" (
+  del /f /q "!targetConfig!\!fileToDelete!"
+)
+
+rem Copy file experts.ini moi
+if exist "!sourceFile!" (
+  copy /y "!sourceFile!" "!targetConfig!\" >nul
+)
+
+:: ==============================
+
+pause
